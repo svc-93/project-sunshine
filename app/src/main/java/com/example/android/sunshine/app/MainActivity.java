6c146477106070ddf0fccae9f6cf8f0541be017e
Vignesh <vigneshchockalingam@gmail.com>
2016-03-20 20:44:47 +0530

    Configured network settings for OpenWeatherMapApi

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.sunshine.app;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +12,12 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -96,6 +103,62 @@ public class MainActivity extends ActionBarActivity {
             // Attach adapter to ListView
             ListView listView = (ListView) rootView.findViewById(R.id.list_view_forecast);
             listView.setAdapter(adapter);
+
+            // Add networking code snippet here
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a String
+            String forecastJson = null;
+
+            try{
+                // Construct the URL for query
+                String baseUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=560024,india&mode=json&units=metric&cnt=7";
+                String apikey = "&APPID=" + BuildConfig.OPEN_WEATHER_MAP_API_KEY;
+                URL url = new URL(baseUrl.concat(apikey));
+
+                // Create request to url and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read input stream
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if(inputStream == null){
+                    // nothing to do
+                    return null;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while((line=reader.readLine())!=null){
+                    // pretty print json for debug logs
+                    buffer.append(line + "\n");
+                }
+
+                if(buffer.length()==0){
+                    // stream is empty, no use parsing
+                    return null;
+                }
+                forecastJson = buffer.toString();
+            } catch(IOException e) {
+                Log.e("PlaceholderFragment", "Error", e);
+                // if code didn't retrieve the weather data, no use parsing
+                return null;
+            } finally {
+                if(urlConnection != null){
+                    urlConnection.disconnect();
+                }
+                if(reader != null){
+                    try{
+                        reader.close();
+                    } catch (final IOException e){
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
 
             return rootView;
         }
